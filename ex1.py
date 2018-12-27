2575881580083ca1344a80d41a8a2ca3ab9150da
Ken English <ken.english@epicure.com>
2018-12-26 21:26:22 -0800

    Up to lesson 3

---
+++ ex1.py
@@ -0,0 +1,8 @@
+print "Hello WOrld!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "Said" do not touch this.'
+
